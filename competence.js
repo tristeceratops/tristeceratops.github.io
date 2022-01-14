c7b366570761add5cfeb92c6b2654f0a137f036e
Eric <eric.woillard@gmail.com>
2022-01-14 18:36:40 +0100

    v1.0

--- competence.js
+++ competence.js
@@ -1,19 +1,14 @@
-var btnSect1 = document.getElementById('btnSect1');
 var btnSect2 = document.getElementById('btnSect2');
 var btnSect3 = document.getElementById('btnSect3');
 var btnSect4 = document.getElementById('btnSect4');
-var btnSect5 = document.getElementById('btnSect5');
-var btnSect6 = document.getElementById('btnSect6');
 
-var section1 = document.getElementById("section1");
+
 var section2 = document.getElementById("section2");
 var section3 = document.getElementById("section3");
 var section4 = document.getElementById("section4");
-var section5 = document.getElementById("section5");
-var section6 = document.getElementById("section6");
 
-var btnSectArray = [btnSect1,btnSect2,btnSect3,btnSect4,btnSect5,btnSect6];
-var SectArray = [section1,section2,section3,section4,section5,section6];
+var btnSectArray = [btnSect2,btnSect3,btnSect4];
+var SectArray = [section2,section3,section4];
 
 
 for (let index = 0; index < btnSectArray.length; index++) {
@@ -23,3 +18,4 @@ for (let index = 0; index < btnSectArray.length; index++) {
     })
     
 }
+
