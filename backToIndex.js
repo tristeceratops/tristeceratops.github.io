afe27a7835dd93e2839748662fb66a8858836d28
Eric <eric.woillard@gmail.com>
2022-01-14 18:43:31 +0100

    i hpe the end

--- backToIndex.js
+++ backToIndex.js
@@ -2,7 +2,7 @@ var btnSect1 = document.getElementById('btnSect1');
 var textBtnSect1 = document.getElementById("textBtnSect1");
 btnSect1.style.cursor="pointer";
 btnSect1.addEventListener("click",function() {
-    document.location.href="index.html";
+    document.location.href="../index.html";
 })
 
 btnSect1.addEventListener("mouseover",function() {
