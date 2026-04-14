8eae5409929e63f6637f4806323907629f7c42ad
Eric <eric.woillard@gmail.com>
2026-04-14 18:41:04 +0200

    shake effect on welcome label

--- src/WelcomePage/WelcomeTextLabel.jsx
+++ src/WelcomePage/WelcomeTextLabel.jsx
@@ -3,5 +3,5 @@ import { useLanguage } from '../i18n/LanguageContext'
 export default function WelcomeTextLabel({ textKey }) {
 	const { t } = useLanguage()
 
-	return <h1 className="welcome-text-label">{t(textKey)}</h1>
+	return <h1 className="welcome-text-label" data-text={t(textKey)}>{t(textKey)}</h1>
 }
